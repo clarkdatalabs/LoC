73b10a653e11905059b7ccd0a9fa8648c7b2ae51
Daniel Tanner <danjtanner@gmail.com>
2018-01-12 13:08:06 -0500

    clean up scratch files

---
+++ LatLong_mapping/locations_to_db.py
@@ -0,0 +1,13 @@
+'''
+Created on Jan 12, 2018
+@author: DJT
+
+This script adds a new table Location Location_v2, to LoC.db.
+This table is imported from Location_joined_cleaned.csv, which consists of 
+
+
+
+'''
+import sqlite3
+import pandas as pd
+from sqlalchemy import create_engine
