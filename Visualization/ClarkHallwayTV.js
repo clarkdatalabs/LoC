1ec46bcedd5a6a6ba86983d38b4edf0e43cc6f36
Daniel Tanner <danjtanner@gmail.com>
2018-04-27 12:23:20 -0400

    add clark hallway version

--- Visualization/ClarkHallwayTV.js
+++ Visualization/ClarkHallwayTV.js
@@ -1,6 +1,6 @@
 
 //define basic visualization parameters
-var startYear = 1400;
+var startYear = 1900; //1400 is a good early start
 	endYear = 2010;
 	timeStep = 100;
 	year = startYear;
@@ -11,29 +11,19 @@ var startYear = 1400;
 //define map size based on screen		
 var mapWidth = mapBox.clientWidth;
 	mapHeight = mapBox.clientHeight;
-	histWidth = histBox.clientWidth;
-	histHeight = histBox.clientHeight;
 
-console.log("width", mapWidth)
-console.log("height", mapHeight)
+//define global variable to track which country is selected
+var selectionID = null;
 
-
-var hist = d3.select("#hist")
-	.append("g")
-	
-console.log("histWidth", histWidth)
-console.log("hist SVG width", hist.clientWidth)
-	
 
 var map = d3.select("#map")
 	.append("g")
 
-
-
+	
 /*Create a new projection using mercator (geoMercator)
 and center it (translate)*/
 var projection = d3.geoMercator()
-	.translate([mapWidth/2, mapHeight/1.8])	/*center in our visual*/
+	.translate([mapWidth/2, mapHeight/1.65])	/*center in our visual*/
 	.scale(mapWidth/6.3)					/*initial scale factor for the geojson map I'm using*/
 
 //create a path using (geoPath) using the projection
@@ -49,6 +39,8 @@ d3.selection.prototype.moveToFront = function() {
   });
 };
 
+
+//		==TOOLTIP==
 
 //Prep the tooltip bits, initial display is hidden (copied from http://bl.ocks.org/mstanaland/6100713)
 var tooltip = d3.select("#map")
@@ -67,19 +59,21 @@ tooltip.append("text")
   .style("text-anchor", "left")
   .attr("font-size", "12px")
   .attr("font-weight", "bold");
+  
 
 /*Read in our data:
 	1. our topojson world definitions
 	2. record counts that have been smoothed over a 5 year window
 	3. a lookup table for country names (they weren't included in our topojson data) */
 d3.queue()
-	.defer(d3.json, "../data/world.json")
-	.defer(d3.csv, "../data/location_by_year_smooth.csv")
-	.defer(d3.csv, "../data/countries.csv")
+	.defer(d3.json, "data/world.json")
+	.defer(d3.csv, "data/location_by_year_smooth.csv")
+	.defer(d3.csv, "data/countries.csv")
+	.defer(d3.csv, "data/year_count.csv")
 	.await(ready);
 	
 /*Once map and LoC data are loaded, do the following*/
-function ready (error, data, LoC, countryLookup) {
+function ready (error, data, LoC, countryLookup, yearCount) {
 	//convert counts to integers
 	LoC.forEach(function(d){
 		d.smooth5 = +d.smooth5;
@@ -99,7 +93,9 @@ function ready (error, data, LoC, countryLookup) {
 					if (LoCData[d.ISOnumeric3] == undefined){
 						LoCData[d.ISOnumeric3] = {}
 					}
-					LoCData[d.ISOnumeric3][parseInt(d.pubDate)]= countryColor(d.count)
+					LoCData[d.ISOnumeric3][parseInt(d.pubDate)]= {}
+					LoCData[d.ISOnumeric3][parseInt(d.pubDate)]["color"] = countryColor(d.count)
+					LoCData[d.ISOnumeric3][parseInt(d.pubDate)]["count"] = parseInt(d.count)
 		}
 	)
 
@@ -112,9 +108,13 @@ function ready (error, data, LoC, countryLookup) {
 		country[d.ISOnumeric3] = d.countryName
 		}
 	)
-
 	
-/*
+	//build year_count lookup table
+	var year_count = {};
+	yearCount.forEach( function(d){
+		year_count[d.year] = d.recordCount
+	}
+	)
 
 	//LOG DATA TO CONSOLE
 	console.log("world.json data:")
@@ -124,9 +124,11 @@ function ready (error, data, LoC, countryLookup) {
 	console.log("LoCData:")
 	console.log(LoCData)
 	console.log("country:")
-	console.log(country);
+	console.log(country)
+	console.log("yearCount:")
+	console.log(yearCount);
 	
-*/
+
 
 
 	//extract country features and draw initial country shapes
@@ -139,10 +141,13 @@ function ready (error, data, LoC, countryLookup) {
 		.attr("fill", null)
 		.attr("fill", noDataColor)
 		
-		//TOOLTIP
+
+		//Define hover and click behavior - highlighting and tooltip
+		
 		//add the class 'highlighted' on mouseover
 		.on('mouseover', function(d) {
 			d3.select(this)
+				//	==HIGHLIGHT==
 				.classed("highlighted", true)
 				.moveToFront();
 			d3.selectAll(".selected")	//bring selected country to the front
@@ -154,12 +159,14 @@ function ready (error, data, LoC, countryLookup) {
 			var dim = tooltip.select("text").node().getBBox();
 			tooltip.select("rect").attr("width", dim.width+10);
 		})
+		
 		//remove the class 'highlighted' on mouseout
 		.on('mouseout', function(d) {
 			d3.select(this)
 				.classed("highlighted", false);
 			tooltip.style("display", "none"); //hide tooltip on mousout
 		})
+		
 		//move tooltip to follow mouse
 		.on('mousemove', function(d) {
 			var xPosition = d3.mouse(this)[0] + 10;
@@ -167,16 +174,18 @@ function ready (error, data, LoC, countryLookup) {
 			tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
 
 		})
+
 		
 		//add the class 'selected' on click
 		.on('click', function(d){
 			console.log(country[parseInt(d.id)]);
-			var selectionName = "Global"
+			selectionID = null;
 			var clickSelection = d3.select(this)
 			clickSelection.classed("selected", function(d){
 				if (clickSelection.classed("selected")){
 					return false	//if already selected, unselect it
 				} else {
+					selectionID = parseInt(d.id);
 					selectionName = country[parseInt(d.id)];
 					d3.selectAll(".selected").classed("selected", false);	//clear previous selection
 					clickSelection.moveToFront()	//bring selected country to front of draw order
@@ -184,7 +193,7 @@ function ready (error, data, LoC, countryLookup) {
 			});
 			console.log("clicky message:", selectionName)
 			tooltip.moveToFront();
-			countryName.textContent = selectionName;
+			//header.textContent = selectionName;
 		})
 		
 	
@@ -195,7 +204,24 @@ function ready (error, data, LoC, countryLookup) {
 		console.log("year:", year)
 		//year = (Math.floor(elapsed/timeStep) % (endYear - startYear)) +startYear
 		
+			
+		
 		yearBox.textContent = year
+		
+		//change header to display count of records of selected country
+		if (selectionID == null){
+			header.textContent = "Global LoC book records with subject location metadata: " 
+				+ parseInt(year_count[year])
+					.toLocaleString();
+			} else { 
+			if (LoCData[selectionID][year] != undefined){ 
+				var selectionCount = LoCData[selectionID][year]["count"];
+			}	else { var selectionCount = 0 }
+			header.textContent = "LoC book records about " 
+				+ country[selectionID] + ": " 
+				+ selectionCount.toLocaleString()
+			}
+				
 		
 				
 			//change fill color to scale with count
@@ -204,7 +230,7 @@ function ready (error, data, LoC, countryLookup) {
 				if (LoCData[parseInt(d.id)] != undefined){
 					if (LoCData[parseInt(d.id)][year] != undefined){
 						//console.log(LoCData[parseInt(d.id)][parseInt(year)])
-						return LoCData[parseInt(d.id)][year];
+						return LoCData[parseInt(d.id)][year]["color"];
 					} else {return noDataColor}
 				} else {return noDataColor}
 			})
